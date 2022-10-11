6d72da5cf5ef15e8b7c0b8e5e98089b19ee21ba8
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-10-11 12:34:31 +0100

    Add test for method create_space in SpaceRepository

--- spec/space_repository_spec.rb
+++ spec/space_repository_spec.rb
@@ -10,20 +10,40 @@ end
 
 
 describe SpaceRepository do 
-    before(:each) do
-        reset_table
+  before(:each) do
+    reset_table
+  end
+
+   describe '#all' do
+     it 'returns all spaces' do
+        repo = SpaceRepository.new
+        all_spaces = repo.all
+
+        expect(all_spaces.length).to eq(2)
+        expect(all_spaces.first.id).to eq(1)
+        expect(all_spaces.first.name).to eq('The Coza, Joshua Tree')
+        expect(all_spaces.first.price_per_night).to eq(145)
+      end
     end
 
-    describe '#all' do
-        it 'returns all spaces' do
-            repo = SpaceRepository.new
-            all_spaces = repo.all
+    describe '#create_space' do
+      repo = SpaceRepository.new
+      space = Space.new
+      space.name = 'Treehouse'
+      space.description = 'Lovely place in the forest'
+      space.price_per_night = 120
+      space.available_dates = '12/03/2023, 12/04/2023'
+      space.user_id = 1
 
-            expect(all_spaces.length).to eq(2)
-            expect(all_spaces.first.id).to eq(1)
-            expect(all_spaces.first.name).to eq('The Coza, Joshua Tree')
-            expect(all_spaces.first.price_per_night).to eq(145)
-        end
+      repo.create_space(space)
+  
+      all_spaces = repo.all
+
+      expect(all_spaces.length).to eq 3
+      expect(all_spaces.last.name).to eq 'Treehouse'
+      expect(all_spaces.last.description).to eq'Lovely place in the forest'
+      expect(all_spaces.last.price_per_night).to eq 120
+      expect(all_spaces.last.available_dates).to eq '12/03/2023, 12/04/2023'
     end
 end
 
