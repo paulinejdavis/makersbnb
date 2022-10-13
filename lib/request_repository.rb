0e41435eb50bb6eb6ab1b0368dc2c151137cabd0
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-10-13 17:21:30 +0100

    create & approve requests

--- lib/request_repository.rb
+++ lib/request_repository.rb
@@ -1,6 +1,40 @@
 require 'request'
+require 'space'
 
 class RequestRepository
+
+    def all
+        requests = []
+
+        sql = 'SELECT * FROM requests;'
+        result_set = DatabaseConnection.exec_params(sql, [])
+        result_set.each do |record|
+            request = Request.new
+            request.id = record['id'].to_i
+            request.approved = record['approved']
+            request.requested_date = record['requested_date']
+            request.user_id = record['user_id'].to_i
+            request.space_id = record['space_id'].to_i
+            
+            requests << request
+        end
+        return requests
+    end
+
+    def find(id)
+        sql = 'SELECT * FROM requests WHERE id = $1;'
+        result_set = DatabaseConnection.exec_params(sql, [id])
+
+        request = Request.new
+        request.id = result_set[0]['id'].to_i
+        request.approved = result_set[0]['approved']
+        request.requested_date = result_set[0]['requested_date']
+        request.user_id = result_set[0]['user_id'].to_i
+        request.space_id = result_set[0]['space_id'].to_i
+
+        return request
+    end
+
     def all_by_user(id_from_user)
         requests = []
 
@@ -21,21 +55,44 @@ class RequestRepository
 
     def all_for_user(id_from_posting_user)
         requests = []
+        
+        spaces = []
 
         sql = 'SELECT * FROM spaces WHERE user_id = $1;'
-        result_set = DatabaseConnection.exec_params(sql, [id_from_user])
+        result_set = DatabaseConnection.exec_params(sql, [id_from_posting_user])
         result_set.each do |record|
+            space = Space.new
+            space.id = record['id'].to_i
+            space.user_id = record['user_id'].to_i
+
+            spaces << space
+        end
+        
+        spaces.each do |space|
+          sql = 'SELECT * FROM requests WHERE space_id = $1;'
+          result_set = DatabaseConnection.exec_params(sql, [space.id])
+          result_set.each do |record|
             request = Request.new
             request.id = record['id'].to_i
             request.approved = record['approved']
             request.requested_date = record['requested_date']
             request.user_id = record['user_id'].to_i
             request.space_id = record['space_id'].to_i
-            
+        
             requests << request
+          end
         end
         return requests
     end
 
+    def create_request(new_request)
+      sql = 'INSERT INTO requests (approved, requested_date, user_id, space_id) VALUES ($1, $2, $3, $4);'
+      DatabaseConnection.exec_params(sql, [new_request.approved, new_request.requested_date, new_request.user_id, new_request.space_id])
+    end
+
+    def approve_request(id)
+        sql = 'UPDATE requests SET approved = true WHERE id = $1;'
+        DatabaseConnection.exec_params(sql, [id])
+    end
 
 end
--- spec/request_repository_spec.rb
+++ spec/request_repository_spec.rb
@@ -12,6 +12,30 @@ describe RequestRepository do
         reset_request_table
     end
 
+    describe '#all' do
+        it 'finds all requests' do
+            repo = RequestRepository.new
+
+            result = repo.all
+
+            expect(result.length).to eq(3)
+            expect(result.first.id).to eq(1)
+            expect(result.first.requested_date).to eq('16/10/2022')
+            expect(result.first.space_id).to eq(2)  
+         end
+    end
+
+    describe "#find" do
+      it "finds a request by id" do
+        repo = RequestRepository.new
+
+        result = repo.find(2)
+
+        expect(result.requested_date).to eq('11/10/2022')
+        expect(result.user_id).to eq(3)
+      end
+    end
+
     describe '#all_by_user' do
         it 'finds all requests made by user' do
             repo = RequestRepository.new
@@ -49,4 +73,39 @@ describe RequestRepository do
       end
     end
 
+    describe '#create_request' do
+        it 'adds a new request' do
+            repo = RequestRepository.new
+
+            new_request = Request.new
+            new_request.approved = 'true'
+            new_request.requested_date = '18/10/2022'
+            new_request.user_id = 1
+            new_request.space_id = 2
+
+            repo.create_request(new_request)
+
+            result = repo.all
+
+            expect(result.length).to eq 4
+            expect(result.last.id).to eq 4
+            expect(result.last.approved).to eq 'true'
+            expect(result.last.requested_date).to eq '18/10/2022'
+            expect(result.last.user_id).to eq 1
+            expect(result.last.space_id).to eq 2
+        end
+    end
+
+    describe '#approve_request' do
+        it 'changes request to approved' do
+            repo = RequestRepository.new
+
+            repo.approve_request(3)
+
+            result = repo.find(3)
+            expect(result.approved).to eq 'true'
+            expect(result.requested_date).to eq '13/10/2022'
+        end
+
+    end
 end
